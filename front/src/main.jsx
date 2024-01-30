9139d1cb7a4a372753d2b3558a0458bcfea16e33
Hugo Ribeiro Alves <89708196+HugoRibeiro-estudante@users.noreply.github.com>
2024-01-30 15:00:25 -0300

    Update main.jsx

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,63 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { RouterProvider, Routes, Route, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './routes/acess/Login.jsx';
 import Register from './routes/acess/Register.jsx';
 import NewBook from './routes/book/NewBook.jsx';
 import BookView from './routes/book/BookView.jsx';
 import NewAnnotation from './routes/annotation/NewAnnotation.jsx';
 import AnnotationView from './routes/annotation/AnnotationView.jsx';
-import PrivateRoute from './routes/acess/PrivateRoute.jsx'; 
+import PrivateRoute from './routes/acess/PrivateRoute.jsx';
 import Home from './routes/Home.jsx';
 import { Profile } from './routes/user/Profile.jsx';
 
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Login />,
-      },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/register',
-        element: <Register />,
-      },
-      {
-        path: '/home/*',
-        element: <PrivateRoute element={<Home />} />
-      },
-      {
-        path: '/profile/*',
-        element: <PrivateRoute element={<Profile />} />
-      },
-      {
-        path: '/newbook/*',
-        element: <PrivateRoute element={<NewBook />} />
-      },
-      {
-        path: '/bookview/*',
-        element: <PrivateRoute element={<BookView />} />
-      },
-      {
-        path: '/newannotation/*',
-        element: <PrivateRoute element={<NewAnnotation />} />
-      },
-      {
-        path: '/annotationview/*',
-        element: <PrivateRoute element={<AnnotationView />} />
-      },
-    ],
-  },
-]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home/*" element={<PrivateRoute element={<Home />} />} />
+      <Route path="/profile/*" element={<PrivateRoute element={<Profile />} />} />
+      <Route path="/newbook/*" element={<PrivateRoute element={<NewBook />} />} />
+      <Route path="/bookview/*" element={<PrivateRoute element={<BookView />} />} />
+      <Route path="/newannotation/*" element={<PrivateRoute element={<NewAnnotation />} />} />
+      <Route path="/annotationview/*" element={<PrivateRoute element={<AnnotationView />} />} />
+    </Routes>
+  </BrowserRouter>
 );
