27d2335e15f253efa6c4ab4288b7a8210ceaa4a6
Hugo Ribeiro Alves <hugo.alves@estudante.iftm.edu.br>
2024-01-30 12:27:37 +0000

    testando ajuste em Link navMenu

--- front/src/components/common/NavMenu.jsx
+++ front/src/components/common/NavMenu.jsx
@@ -3,6 +3,7 @@ import Home from '../../assets/img/Home.png';
 import User from '../../assets/img/User.png';
 import Book from '../../assets/img/Book.png';
 import Rank from '../../assets/img/Rank.png';
+import { Link } from 'react-router-dom';
 
 export const NavMenu = ({ activeChild }) => {
     const navMenuStyle = {
@@ -45,10 +46,10 @@ export const NavMenu = ({ activeChild }) => {
     return (
         <div style={navMenuStyle.container}>
             <div style={navMenuStyle.nav}>
-                <a href="/newbook"><img src={Book} alt="" style={activeChild === 1 ? navMenuStyle.active : navMenuStyle.img} /></a>
+                <Link to="/newbook"><img src={Book} alt="" style={activeChild === 1 ? navMenuStyle.active : navMenuStyle.img} /></Link>
                 <a href="#"><img src={Rank} alt="" style={activeChild === 2 ? navMenuStyle.active : navMenuStyle.img} /></a>
-                <a href="/home"><img src={Home} alt="" style={activeChild === 3 ? navMenuStyle.active : navMenuStyle.img} /></a>
-                <a href="/profile"><img src={User} alt="" style={activeChild === 4 ? navMenuStyle.active : navMenuStyle.img} /></a>
+                <Link to="/home"><img src={Home} alt="" style={activeChild === 3 ? navMenuStyle.active : navMenuStyle.img} /></Link>
+                <Link to="profile"><img src={User} alt="" style={activeChild === 4 ? navMenuStyle.active : navMenuStyle.img} /></Link>
             </div>
         </div>
     );
