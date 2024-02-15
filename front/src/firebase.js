ad99f6ead0e20043516275bd3b4e86a83f4c1499
Hugo Ribeiro Alves <89708196+HugoRibeiro-estudante@users.noreply.github.com>
2024-02-15 11:29:19 -0300

    Update firebase.js

--- front/src/firebase.js
+++ front/src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAUAuXgYqj6MwJneuFLpgVtM5h-JXNciOU",
-    authDomain: "lenoute-1fefd.firebaseapp.com",
-    projectId: "lenoute-1fefd",
-    storageBucket: "lenoute-1fefd.appspot.com",
-    messagingSenderId: "95768995426",
-    appId: "1:95768995426:web:69692806f5d3a4219f933b"
+    //CREDENCIAIS
   };
   
   const app = initializeApp(firebaseConfig);
